2aa347e2d1afeb35299d759fd759cc2bdff9f672
hilmimughid <113970790+hilmimughid@users.noreply.github.com>
2023-09-21 20:05:51 +0700

    Pertemuan 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,55 @@
-// Impor package Flutter Material Design.
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; // Mengimpor paket material.dart dari Flutter
 
-// Fungsi main yang menjalankan aplikasi.
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp()); // Menjalankan aplikasi Flutter
 }
 
-// Widget utama aplikasi.
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  // Membuat class MyApp yang merupakan widget tanpa state
+  const MyApp({super.key}); // Constructor untuk class MyApp
 
-  // Fungsi build untuk membangun widget.
   @override
   Widget build(BuildContext context) {
+    // Method build untuk membangun widget
     return MaterialApp(
-      title: 'Flutter Demo',
+      // Mengembalikan MaterialApp sebagai root dari widget tree
+      title: 'Hilmi Mughid App', // Judul aplikasi
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
-        useMaterial3: true,
+        // Tema aplikasi
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.lightBlue), // Skema warna aplikasi
+        useMaterial3: true, // Menggunakan Material versi 3
       ),
-      home: const MyHomePage(title: 'Hilmi App'),
+      home:
+          const MyHomePage(title: 'Hilmi Mughid App'), // Halaman utama aplikasi
     );
   }
 }
 
-// Halaman utama aplikasi.
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  // Membuat class MyHomePage yang merupakan widget dengan state
+  const MyHomePage(
+      {super.key, required this.title}); // Constructor untuk class MyHomePage
 
-  final String title;
+  final String title; // Judul halaman
 
-  // Membuat state untuk widget ini.
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHomePage> createState() =>
+      _MyHomePageState(); // Membuat state untuk MyHomePage
 }
 
-// State untuk MyHomePage.
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // Membuat class _MyHomePageState sebagai state dari MyHomePage
+  int _counter = 0; // Variabel untuk menyimpan nilai counter
 
-  // Fungsi untuk menambah counter.
+  // Method untuk menambah counter
   void _incrementCounter() {
     setState(() {
       _counter++;
     });
   }
 
-  // Fungsi untuk mengurangi counter.
+  // Method untuk mengurangi counter
   void _decrementCounter() {
     setState(() {
       if (_counter > 0) {
@@ -55,32 +58,48 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  // Fungsi untuk mereset counter.
+  // Method untuk mereset counter
   void _resetCounter() {
     setState(() {
       _counter = 0;
     });
   }
 
-  // Membangun tampilan aplikasi.
   @override
   Widget build(BuildContext context) {
+    // Method build untuk membangun widget dengan state
     return Scaffold(
+      // Mengembalikan Scaffold sebagai root dari widget tree dengan state
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        // AppBar aplikasi
+        backgroundColor: Theme.of(context)
+            .colorScheme
+            .inversePrimary, // Warna latar belakang AppBar
+        title: Text(widget.title), // Judul AppBar
       ),
       body: Stack(
+        // Body aplikasi berupa Stack widget
         children: <Widget>[
           Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
+                // Menampilkan gambar avatar
+                Image.asset(
+                  'assets/images/avatar.jpg',
+                  width: 200,
+                  height: 200,
+                ),
                 const Text(
                   'Hilmi Mughid',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
                 const Text(
                   '2141720081\n',
+                  style: TextStyle(fontSize: 20),
                 ),
                 Text(
                   '$_counter',
@@ -96,16 +115,19 @@ class _MyHomePageState extends State<MyHomePage> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
+                  // Tombol untuk mereset counter
                   FloatingActionButton(
                     onPressed: _resetCounter,
                     tooltip: 'Reset',
                     child: const Icon(Icons.refresh),
                   ),
+                  // Tombol untuk mengurangi counter
                   FloatingActionButton(
                     onPressed: _decrementCounter,
                     tooltip: 'Decrement',
                     child: const Icon(Icons.remove),
                   ),
+                  // Tombol untuk menambah counter
                   FloatingActionButton(
                     onPressed: _incrementCounter,
                     tooltip: 'Increment',
