377240e7605ab5e01448423e15568e9c25daabda
hilmimughid <113970790+hilmimughid@users.noreply.github.com>
2023-10-30 19:32:28 +0700

    UTS

--- lib/main.dart
+++ lib/main.dart
@@ -10,60 +10,57 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hilmi Mughid 2141720081',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Hilmi Mughid 2141720081'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        title: 'Hilmi Mughid 2141720081',
+        home: Scaffold(
+          appBar: AppBar(title: const Text('Hilmi Mughid 2141720081')),
+          body: Container(
+            padding: const EdgeInsets.all(20),
+            color: Colors.grey,
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 200,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Container(
+                          color: Colors.red,
+                          child: const Center(
+                              child: Text(
+                            "2",
+                            style: TextStyle(color: Colors.white, fontSize: 30),
+                          )),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          color: Colors.yellow,
+                          child: const Center(
+                              child: Text("3",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 30))),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Expanded(
+                    child: Container(
+                  color: Colors.orange,
+                  child: const Center(
+                      child: Text("4",
+                          style: TextStyle(color: Colors.white, fontSize: 30))),
+                )),
+                Expanded(
+                    child: Container(
+                  color: Colors.brown,
+                  child: const Center(
+                      child: Text("5",
+                          style: TextStyle(color: Colors.white, fontSize: 30))),
+                )),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+          ),
+        ));
   }
 }
