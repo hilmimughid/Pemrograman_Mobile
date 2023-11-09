be202a3cdb5e92a06981d460aef33cd8e97227a6
hilmimughid <113970790+hilmimughid@users.noreply.github.com>
2023-11-09 19:58:30 +0700

    Pertemuan 11

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
