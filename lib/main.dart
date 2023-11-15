91b2ae2371a351c9492585487ca241508027274e
hilmimughid <113970790+hilmimughid@users.noreply.github.com>
2023-11-15 21:25:58 +0700

    coba

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,51 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: MyHomePage(),
+    );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final List<String> data = ["Hilmi", "Mughid", "Fahmi", "Amar", "Fais"];
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<String> data = ["Hilmi", "Mughid", "Fahmi", "Amar", "Faizi"];
+  String? dropdownValue;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Dropdown"),
+        title: const Text("Hilmi Mughid 2141720081"),
       ),
       body: Center(
-        child: DropdownButton(
-            hint: Text("Pilih..."),
-            onChanged: (value) {
-              print(value);
-            },
-            items: data
-                .map((e) => DropdownMenuItem(value: e, child: Text(e)))
-                .toList()),
+        child: DropdownButton<String>(
+          value: dropdownValue,
+          hint: const Text("Pilih..."),
+          onChanged: (String? newValue) {
+            setState(() {
+              dropdownValue = newValue;
+            });
+          },
+          items: data.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
